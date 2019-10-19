050038ad192a12279a1a2ea21807402287aa9bee
Unknown <eugene.choy.2016@sis.smu.edu.sg>
2019-10-19 13:58:11 +0800

    Searchbox

--- webapp/src/screen/home/TemporaryDrawer.js
+++ webapp/src/screen/home/TemporaryDrawer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade,makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
@@ -15,15 +15,50 @@ import { Typography } from '@material-ui/core';
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/Toolbar";
 import StarIcon from '@material-ui/icons/Star';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { mergeClasses } from '@material-ui/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme=>({
   list: {
     width: 250,
   },
   fullList: {
     width: 'auto',
   },
-});
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  }
+}));
 
 export default function TemporaryDrawer() {
   const classes = useStyles();
@@ -88,7 +123,13 @@ export default function TemporaryDrawer() {
                         <MenuIcon onClick={toggleDrawer('left', true)} style={{color: 'white',marginLeft:15}}  />
                         <Typography class="whiteText">Find Fud 4 Me</Typography>
                     </IconButton>
-                        
+
+                            <InputBase style={{marginLeft:15}}
+                              placeholder="Search…"
+                              inputProps={{ 'aria-label': 'search' }}
+                            />
+
+                      
                     </ToolBar>
                 </AppBar>
       <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
