d90fcac4befac2e44301acf3cdc4830d0f574191
Unknown <eugene.choy.2016@sis.smu.edu.sg>
2019-10-19 14:49:52 +0800

    UI

--- webapp/src/screen/home/BaseView.jsx
+++ webapp/src/screen/home/BaseView.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import MapView from "./MapView";
-
+import Fab from '@material-ui/core/Fab';
 import ListView from "./ListView";
-
 import TemporaryDrawer from './TemporaryDrawer';
-
+import ListIcon from '@material-ui/icons/List';
 
 
 class BaseView extends React.Component {
@@ -12,18 +11,36 @@ class BaseView extends React.Component {
         super(props);
 
         this.state = ({
-            nearbyFoodPlaces: []
+            nearbyFoodPlaces: [],
+            showList: false
         });
 
         this.onNearbyFoodPlacesChange = this.onNearbyFoodPlacesChange.bind(this);
     }
 
+
+    showList = () =>{
+        console.log("setted")
+        this.setState({
+            showList: true
+        })
+    }
+
+
     render() {
         return (
             <div className="AppContainer">
                 <TemporaryDrawer></TemporaryDrawer>
-                <ListView nearbyFoodPlaces={this.state.nearbyFoodPlaces}/>
+
+                {
+                    this.state.showList ? <ListView nearbyFoodPlaces={this.state.nearbyFoodPlaces}/> : null
+                }
+
                 <MapView onNearbyFoodPlacesChangeListener={this.onNearbyFoodPlacesChange}/>
+                <div class="bottomRightBtn">
+                <Fab onClick={this.showList} style={{margin:5}} color="primary" aria-label="add"><ListIcon /></Fab>
+                </div>
+
             </div>
 
         );
--- webapp/src/screen/home/TemporaryDrawer.js
+++ webapp/src/screen/home/TemporaryDrawer.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 import { fade,makeStyles } from '@material-ui/core/styles';
-
-import { makeStyles } from '@material-ui/core/styles';
-
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
