70b81fd903c7d94a5c858ad3bacf7438c92d8f34
Wei Hong <weihong.ho.2016@smu.edu.sg>
2019-10-19 13:36:35 +0800

    Add List view

--- webapp/src/screen/home/BaseView.jsx
+++ webapp/src/screen/home/BaseView.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import MapView from "./MapView";
+import ListView from "./ListView";
 
 class BaseView extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = ({
+            nearbyFoodPlaces: []
+        });
+
+        this.onNearbyFoodPlacesChange = this.onNearbyFoodPlacesChange.bind(this);
     }
 
     render() {
         return (
             <div>
                 <h1>Hi</h1>
-                <MapView/>
+                <ListView nearbyFoodPlaces={this.state.nearbyFoodPlaces}/>
+                <MapView onNearbyFoodPlacesChangeListener={this.onNearbyFoodPlacesChange}/>
             </div>
 
         );
     }
 
+    onNearbyFoodPlacesChange(nearbyFoodPlaces) {
+        this.setState({
+            nearbyFoodPlaces: nearbyFoodPlaces
+        })
+    }
 }
 
 export default BaseView;
---
+++ webapp/src/screen/home/ListView.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Container from "@material-ui/core/Container";
+import ListObject from "./ListObject";
+
+class ListView extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        if(!this.props.nearbyFoodPlaces) {
+            return <React.Fragment/>
+        } else {
+            console.log(this.props.nearbyFoodPlaces);
+            return (
+                <React.Fragment>
+                    <Container className="listContainer">
+                        {
+                            this.props.nearbyFoodPlaces.map(nearbyFoodPlace => <ListObject nearbyFoodPlace={nearbyFoodPlace}/>)
+                        }
+                    </Container>
+                </React.Fragment>
+            )
+        }
+    }
+
+}
+
+export default ListView;
