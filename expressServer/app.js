b3c1130ce8c98a256ba092edc52da391e4c68303
Wei Hong <weihong.ho.2016@smu.edu.sg>
2019-10-19 11:56:31 +0800

    Disable CORS

--- expressServer/app.js
+++ expressServer/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3001
 const path = require('path');
 
-
+app.use(cors());
 
 var loc = require('./locations')
 app.use('/locations',loc) //Add new file to path
